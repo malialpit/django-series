b9a64858b4656611236e594a058d331648d518b0
Alpit Mali <arpitmali888@gmail.com>
2025-07-30 01:49:48 +0530

    #6_reset_change_password functionality added.

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,6 @@ app_name = 'accounts'
 urlpatterns = [
     path('dashboard', views.dashboard, name='dashboard'),
     path('login/', views.login_view, name='login'),
-    path('', views.signup, name='signup')
+    path('', views.signup, name='signup'),
+    path('admin-change-password/', views.admin_change_password, name='admin-change-password'),
 ]
